c434981c2dcc3d79d10d287f338bbb592c501f14
Khong <hankhongg@gmail.com>
2025-03-18 21:05:42 +0700

    feat: controllers for users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    // find all users GET /users
+    @Get()
+    findAll(){
+        return 'This action returns all users';
+    }
+    // find user by id GET /users/:id
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return {id};
+    }
+    // create user POST /users
+    @Post()
+    create(@Body() user: {}){
+        return user;
+    }
+    // update user partially Patch /users/:id
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() user: {}){
+        return {id, ...user};
+    }
+    // delete user DELETE /users/:id
+    @Delete(':id')
+    delete(@Param('id') id: string){
+        return `This action removes a #${id} user`;
+    }
+}
