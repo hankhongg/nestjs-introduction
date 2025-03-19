f488b59a955dadd785bf71e711269479c1fd0707
Khong <hankhongg@gmail.com>
2025-03-19 18:14:45 +0700

    feat: lesson 2 services

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,30 +1,34 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
+// gotta import the service AND put it in the constructor as a bean aka dependency injection
 @Controller('users')
 export class UsersController {
-    // find all users GET /users
+    constructor(private readonly usersService: UsersService){}
+
+    // find all users GET /users [can use query to filter the roles] // http://localhost:3000/users?role=INTERN
     @Get()
-    findAll(){
-        return 'This action returns all users';
+    findAll(@Query('role') role?: 'INTERN' | 'EMPLOYEE'){
+        return this.usersService.findAll(role);
     }
     // find user by id GET /users/:id
     @Get(':id')
     findOne(@Param('id') id: string) {
-        return {id};
+        return this.usersService.findOne(id);
     }
     // create user POST /users
     @Post()
-    create(@Body() user: {}){
-        return user;
+    create(@Body() user: {name: string, email: string, role: 'INTERN' | 'EMPLOYEE'}){
+        return this.usersService.create(user);
     }
     // update user partially Patch /users/:id
     @Patch(':id')
-    update(@Param('id') id: string, @Body() user: {}){
-        return {id, ...user};
+    update(@Param('id') id: string, @Body() user: {name: string, email: string, role: 'INTERN' | 'EMPLOYEE'}){
+        return this.usersService.update(id, user);
     }
     // delete user DELETE /users/:id
     @Delete(':id')
     delete(@Param('id') id: string){
-        return `This action removes a #${id} user`;
+        return this.usersService.delete(id);
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = [
+        {id: 1, name: 'John Doe', email: "john@gmailcom", role: 'EMPLOYEE'},
+        {id: 2, name: 'Jane Doe', email: "jane@gmailcom", role: 'INTERN'},
+        {id: 3, name: 'Jim Doe', email: "jim@gmailcom", role: 'EMPLOYEE'},
+        {id: 4, name: 'Jill Doe', email: "jill@gmailcom", role: 'INTERN'},
+        {id: 5, name: 'Jack Doe', email: "jack@gmailcom", role: 'EMPLOYEE'},
+    ];
+    findAll(role?: 'INTERN' | 'EMPLOYEE'){
+        if (role){
+            return this.users.filter(user => user.role === role);
+        }
+        return this.users;
+    }
+    findOne(id: string) {
+        return this.users.filter(user => user.id === +id);
+    }
+    create(user: {name: string, email: string, role: 'INTERN' | 'EMPLOYEE'}){
+        const sortedUsersById = [...this.users].sort((a,b)=> b.id - a.id) // spread the array so i wont mutate the original array
+        const highestId = sortedUsersById[0].id;
+        const newUser = {id: highestId + 1, ...user};
+        this.users.push(newUser);
+        return newUser;
+    }
+    update(id: string, updatedUser: {name: string, email: string, role: 'INTERN' | 'EMPLOYEE'}){
+        // find the user
+        // if exists update the user
+        // if not return the old one
+        this.users = [...this.users].map((user, i) => {
+            if (user.id === +id){
+                return {...user, ...updatedUser};
+                
+            }
+            return user;
+        });
+        return this.findOne(id);
+    }
+    delete(id: string){
+        return this.users = this.users.filter(user => user.id !== +id);
+        
+    }
+}
